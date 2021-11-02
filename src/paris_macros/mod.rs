79d636d31b49680c7b3a5d549558c1d1f5caac71
Mariusz Bialonczyk <manio@skyboo.net>
2021-11-02 17:56:05 +0100

    paris_macros: add macros documentation from log crate

--- src/paris_macros/mod.rs
+++ src/paris_macros/mod.rs
@@ -1,4 +1,19 @@
-#[allow(missing_docs)]
+/// Logs a message at the info level.
+///
+/// # Examples
+///
+/// ```edition2018
+/// use log::info;
+///
+/// # fn main() {
+/// # struct Connection { port: u32, speed: f32 }
+/// let conn_info = Connection { port: 40, speed: 3.20 };
+///
+/// info!("Connected to port {} at {} Mb/s", conn_info.port, conn_info.speed);
+/// info!(target: "connection_events", "Successfull connection, port: {}, speed: {}",
+///       conn_info.port, conn_info.speed);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! info {
     ($($args:tt)+) => {
@@ -6,7 +21,21 @@ macro_rules! info {
     };
 }
 
-#[allow(missing_docs)]
+/// Logs a message at the debug level.
+///
+/// # Examples
+///
+/// ```edition2018
+/// use log::debug;
+///
+/// # fn main() {
+/// # struct Position { x: f32, y: f32 }
+/// let pos = Position { x: 3.234, y: -1.223 };
+///
+/// debug!("New position: x: {}, y: {}", pos.x, pos.y);
+/// debug!(target: "app_events", "New position: x: {}, y: {}", pos.x, pos.y);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! debug {
     ($($args:tt)+) => {
@@ -14,7 +43,23 @@ macro_rules! debug {
     };
 }
 
-#[allow(missing_docs)]
+/// Logs a message at the trace level.
+///
+/// # Examples
+///
+/// ```edition2018
+/// use log::trace;
+///
+/// # fn main() {
+/// # struct Position { x: f32, y: f32 }
+/// let pos = Position { x: 3.234, y: -1.223 };
+///
+/// trace!("Position is: x: {}, y: {}", pos.x, pos.y);
+/// trace!(target: "app_events", "x is {} and y is {}",
+///        if pos.x >= 0.0 { "positive" } else { "negative" },
+///        if pos.y >= 0.0 { "positive" } else { "negative" });
+/// # }
+/// ```
 #[macro_export]
 macro_rules! trace {
     ($($args:tt)+) => {
@@ -22,7 +67,20 @@ macro_rules! trace {
     };
 }
 
-#[allow(missing_docs)]
+/// Logs a message at the warn level.
+///
+/// # Examples
+///
+/// ```edition2018
+/// use log::warn;
+///
+/// # fn main() {
+/// let warn_description = "Invalid Input";
+///
+/// warn!("Warning! {}!", warn_description);
+/// warn!(target: "input_events", "App received warning: {}", warn_description);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! warn {
     ($($args:tt)+) => {
@@ -30,7 +88,20 @@ macro_rules! warn {
     };
 }
 
-#[allow(missing_docs)]
+/// Logs a message at the error level.
+///
+/// # Examples
+///
+/// ```edition2018
+/// use log::error;
+///
+/// # fn main() {
+/// let (err_info, port) = ("No connection", 22);
+///
+/// error!("Error: {} on port {}", err_info, port);
+/// error!(target: "app_events", "App Error: {}, Port: {}", err_info, 22);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! error {
     ($($args:tt)+) => {
